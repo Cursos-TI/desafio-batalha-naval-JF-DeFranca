f18abdd47b5d973f7ec67a4757567f72b0bf38a7
JF-DeFranca <joaofernando660cc@gmail.com>
2025-04-11 03:11:35 +0000

    Finalização do nível Aventureiro e correção de erros

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,43 +4,93 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 
 
+
+#define TAM 10
+#define AGUA 0
+#define NAVIO 3
+#define TAMANHO_NAVIO 3
+
+
 int main() {
 
-    int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 3, 3, 3, 3, 0, 0, 0, 0}, // NAVIO HORIZONTAL (LINHA 3)
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 3, 0, 0, 0}, // NAVIO VERTICAL (COLUNA G)
-        {0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
-      
-        //IMPRESSÃO DAS COLUNAS A a J 
+    int tabuleiro[TAM][TAM];
 
-     printf("\n    ");
+    // DEFININDO O TABULEIRO COMPLETO COM AGUA (0)
+    for (int i = 0; i < TAM; i++){
+        for (int j = 0; j < TAM; j++){
+            tabuleiro[i][j] = AGUA;
+        }  
+    }
 
-      for (int j = 0; j < 10; j++) {
-             printf(" %c ", 'A' + j);
-        
-            }
-           
-            printf("\n");
+    //NAVIO 1 NA HORIZONTAL
+    for (int j = 0; j < TAMANHO_NAVIO; j++){
+        tabuleiro[0][j] = NAVIO;
 
+    }
+    
+    //NAVIO 2 NA VERTICAL
+    for (int i = 0; i < TAMANHO_NAVIO; i++){
+        tabuleiro[1+i][5] = NAVIO;
 
-         //IMPRESSÃO DAS LINHAS 1 a 10
+    }
+    
+    //NAVIO 3 NA DIAGONAL
 
-      for (int i = 0; i < 10; i++) {
-                printf("%2d  ", i + 1);
-             for (int j = 0; j < 10; j++) {
-                printf(" %d ", tabuleiro[i][j]);
+    for (int i = 0; i < TAMANHO_NAVIO; i++){
+        int linha = 5 + i;
+        int coluna = 1 + i;
+
+        if (linha < TAM && coluna < TAM){
+            tabuleiro [linha][coluna] = NAVIO;
         }
-        
+             
+    }
+
+    //NAVIO 4 NA DIAGONAL INVERSA
+
+    for (int i = 0; i < TAMANHO_NAVIO; i++){
+        int linha = i;
+        int coluna = 9 - i;
+
+        if (linha < TAM && coluna >= 0){
+            tabuleiro [linha][coluna] = NAVIO;
+        }
+              
+    }
+
+    // IMPRESSÃO DO TABULEIRO 
+
+    printf("\nTABULEIRO DE BATALHA NAVAL\n");
+    printf("ÁGUA = 0 NAVIO = 3\n");
+    printf("\n");
+    
+
+    for (int i = 0; i < TAM; i++){
+        for (int j = 0; j < TAM; j++){
+            printf("%2d ", tabuleiro[i][j]);
+            
+        }
         printf("\n");
     }
+    
+
+
+    
+    
+    
+    
+
+
+
+
+
+   
+
+
+
+
+
+
 
     return 0;
 }
